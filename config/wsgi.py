55ce76abe2ba455a36ddd9eaf951702a09f9aafa
sriman <166496128+project-2-2-2@users.noreply.github.com>
2025-02-23 10:05:54 +0530

    Update wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,1 +1,5 @@
-# WSGI entry point placeholder
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+application = get_wsgi_application()
