bbd9a24ee2dc663ec8eff9d7a0badb91513438c6
sriman <166496128+project-2-2-2@users.noreply.github.com>
2025-02-23 10:08:40 +0530

    Update urls.py

--- support/urls.py
+++ support/urls.py
@@ -1,1 +1,6 @@
-# Support URLs
+from django.urls import path
+from .views import AllServiceRequestsView
+
+urlpatterns = [
+    path('all-requests/', AllServiceRequestsView.as_view(), name='all_service_requests'),
+]
