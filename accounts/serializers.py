3f466a55ca4969d2d1e86d22f6a5ac39233be697
sriman <166496128+project-2-2-2@users.noreply.github.com>
2025-02-23 10:06:39 +0530

    Update serializers.py

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,1 +1,7 @@
-# Accounts serializers
+from rest_framework import serializers
+from .models import CustomUser
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'role']
