03664d8a0ab0491fb724a223015595cdf8f41751
sriman <166496128+project-2-2-2@users.noreply.github.com>
2025-02-23 10:06:25 +0530

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,1 +1,12 @@
-# Accounts models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('customer', 'Customer'),
+        ('support', 'Customer Support'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='customer')
+
+    def __str__(self):
+        return self.username
