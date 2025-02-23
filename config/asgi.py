eccdbb63daaffac5690d44c24d8644e46d0eb4b9
sriman <166496128+project-2-2-2@users.noreply.github.com>
2025-02-23 10:06:07 +0530

    Update asgi.py

--- config/asgi.py
+++ config/asgi.py
@@ -1,1 +1,5 @@
-# ASGI entry point placeholder
+import os
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+application = get_asgi_application()
