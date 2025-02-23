967de680d3aea3e84f1955e3fd0a817576008933
sriman <166496128+project-2-2-2@users.noreply.github.com>
2025-02-23 10:08:14 +0530

    Update urls.py

--- service_requests/urls.py
+++ service_requests/urls.py
@@ -1,1 +1,10 @@
-# Service request URLs
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ServiceRequestViewSet
+
+router = DefaultRouter()
+router.register('', ServiceRequestViewSet, basename='service-request')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
