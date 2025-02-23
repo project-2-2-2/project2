4f4146764d21be269d493eb34ae22691c5c03d60
sriman <166496128+project-2-2-2@users.noreply.github.com>
2025-02-23 10:08:04 +0530

    Update views.py

--- service_requests/views.py
+++ service_requests/views.py
@@ -1,1 +1,18 @@
-# Service request views
+from rest_framework import viewsets, permissions
+from .models import ServiceRequest
+from .serializers import ServiceRequestSerializer
+
+class ServiceRequestViewSet(viewsets.ModelViewSet):
+    serializer_class = ServiceRequestSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        # Customers see only their own requests; support staff see all.
+        if user.role == 'customer':
+            return ServiceRequest.objects.filter(user=user)
+        return ServiceRequest.objects.all()
+
+    def perform_create(self, serializer):
+        # Automatically assign the logged-in user as the owner
+        serializer.save(user=self.request.user)
