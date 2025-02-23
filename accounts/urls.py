4766a884cab24328415fc69101f945ac951e7c3a
sriman <166496128+project-2-2-2@users.noreply.github.com>
2025-02-23 10:07:02 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,1 +1,9 @@
-# Accounts URLs
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import UserProfileView
+
+urlpatterns = [
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/', UserProfileView.as_view(), name='user_profile'),
+]
