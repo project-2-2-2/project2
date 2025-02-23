95e92d9958298c3ab1520d87f7c2b57741927f88
sriman <166496128+project-2-2-2@users.noreply.github.com>
2025-02-23 10:08:30 +0530

    Update views.py

--- support/views.py
+++ support/views.py
@@ -1,1 +1,13 @@
-# Support views
+from rest_framework import generics, permissions
+from service_requests.models import ServiceRequest
+from service_requests.serializers import ServiceRequestSerializer
+
+class AllServiceRequestsView(generics.ListAPIView):
+    serializer_class = ServiceRequestSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # Only allow support staff to view all requests
+        if self.request.user.role == 'support':
+            return ServiceRequest.objects.all()
+        return ServiceRequest.objects.none()
