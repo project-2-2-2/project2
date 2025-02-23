9443a8603f9cd7106d25f674e5b70521454aceac
sriman <166496128+project-2-2-2@users.noreply.github.com>
2025-02-23 10:07:52 +0530

    Update serializers.py

--- service_requests/serializers.py
+++ service_requests/serializers.py
@@ -1,1 +1,7 @@
-# Service request serializers
+from rest_framework import serializers
+from .models import ServiceRequest
+
+class ServiceRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ServiceRequest
+        fields = '__all__'
