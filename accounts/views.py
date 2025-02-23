be0b6bc7ebb62cb075d12a51819240b24328d074
sriman <166496128+project-2-2-2@users.noreply.github.com>
2025-02-23 10:06:49 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,10 @@
-# Accounts views
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .serializers import CustomUserSerializer
+
+class UserProfileView(generics.RetrieveAPIView):
+    serializer_class = CustomUserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
